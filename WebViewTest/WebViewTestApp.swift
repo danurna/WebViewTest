b65c1651c1f125fad6741313253edb4245fc6afe
Daniel Witurna <daniel.witurna@hi.health>
2022-06-28 11:38:14 +0200

    Initial Commit

---
+++ WebViewTest/WebViewTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebViewTestApp.swift
+//  WebViewTest
+//
+//  Created by Daniel Witurna on 28.06.22.
+//
+
+import SwiftUI
+
+@main
+struct WebViewTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
